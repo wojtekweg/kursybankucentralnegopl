1caab97f7568a4c9df99634509d7b332b1b52a05
DawidMoza <dawidmoza@gmail.com>
2021-04-29 02:11:14 +0200

    prototyp klasy currency, nowe oraz zmienione testy

--- src/Currency.js
+++ src/Currency.js
@@ -1,13 +1,46 @@
 class Currency {
-  constructor(code, name, value, date) {
-    this.code = code;
-    this.name = name;
-    this.value = value;
-    this.date = date;
-  }
-  convert(currency_in, currency_out) {}
-  get(code, date) {
-    return undefined;
-  }
+	
+	constructor(code, date) {
+		get(code, date);
+	}
+	
+	static convert(currency_in, currency_out, money_in) {
+		//returns amount of money_in in currency_out currency
+	}
+	
+	get(code, date) {
+		this.code = code;
+		this.date = date;
+		changed = false
+		if (//invalid date){
+			changed = true
+			this.date = //date of day before
+			//if date is too old, or from future, get the eldest avaiable, or the newest one
+			//if currency is not aviable by 2-01-2002, and sufficent date wasn't found - set value to nan
+		}
+		if (changed){
+			//inform somehow
+		}
+		this.name = //getname
+		this.value = //exchange rate
+	}
 }
+
+/*function getSite(url){
+        const http = new XMLHttpRequest();
+        http.open("GET", url);
+        http.send();
+        return http;
+    }
+    
+    site = getSite('http://api.nbp.pl/api/exchangerates/rates/a/chf/');
+    site.onreadystatechange = function () {
+        if (site.readyState == 4){
+            doc = JSON.parse(site.responseText).rates[0].mid;
+            document.write(doc)
+        }
+    }
+*/
+
+
 module.exports = Currency;
