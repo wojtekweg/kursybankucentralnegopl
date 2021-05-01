9468d1fc4a1ef34f8476c64d7387c9d290d9b029
DawidMoza <dawidmoza@gmail.com>
2021-05-01 02:25:49 +0200

    poczatki impementacji

--- src/Currency.js
+++ src/Currency.js
@@ -1,46 +1,37 @@
-class Currency {
+﻿class Currency {
 	
 	constructor(code, date) {
-		get(code, date);
+		
 	}
 	
-	static convert(currency_in, currency_out, money_in) {
-		//returns amount of money_in in currency_out currency
+	static convert(currency_in, currency_out) {
+		return currency_in.value / currency_out.value
 	}
 	
 	get(code, date) {
 		this.code = code;
 		this.date = date;
-		changed = false
-		if (//invalid date){
-			changed = true
-			this.date = //date of day before
-			//if date is too old, or from future, get the eldest avaiable, or the newest one
-			//if currency is not aviable by 2-01-2002, and sufficent date wasn't found - set value to nan
+		
+		if (code != "pln"){
+		
+			const http = new XMLHttpRequest();
+			http.open("GET", `http://api.nbp.pl/api/exchangerates/rates/A/${code}/${date}/`);
+			http.send();
+			
+			var parent = this
+			
+			http.onreadystatechange = function () {
+				if (http.readyState == 4){
+					parent.value = JSON.parse(http.responseText).rates[0].mid
+					parent.name = JSON.parse(http.responseText).currency
+				}
+			}
+			
+		} else {
+			this.value = 1
+			this.name = "nowy polski złoty"
 		}
-		if (changed){
-			//inform somehow
-		}
-		this.name = //getname
-		this.value = //exchange rate
 	}
 }
 
-/*function getSite(url){
-        const http = new XMLHttpRequest();
-        http.open("GET", url);
-        http.send();
-        return http;
-    }
-    
-    site = getSite('http://api.nbp.pl/api/exchangerates/rates/a/chf/');
-    site.onreadystatechange = function () {
-        if (site.readyState == 4){
-            doc = JSON.parse(site.responseText).rates[0].mid;
-            document.write(doc)
-        }
-    }
-*/
-
-
-module.exports = Currency;
+//module.exports = Currency;
