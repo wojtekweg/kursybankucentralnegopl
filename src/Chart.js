d29b1ae5aed3df60b0ae4075c6a005b49e9f79aa
DawidMoza <dawidmoza@gmail.com>
2021-05-18 03:57:28 +0200

    wykresy

--- src/Chart.js
+++ src/Chart.js
@@ -5,27 +5,22 @@ class Chart {
 	}
 	
 	get(start, end, currency){
-		console.log("xd")
 		
-		/*var http = new XMLHttpRequest();
+		var http = new XMLHttpRequest();
 		
 		var parent = this;
 		
-		http.open("GET", `http://api.nbp.pl/api/exchangerates/tables/A/${TableNBP.format(this.date)}/`);
+		http.open("GET", `http://api.nbp.pl/api/exchangerates/rates/A/${currency}/${start}/${end}`);
 		
 		http.onreadystatechange = function () {
 			if (http.readyState == 4){
 				if (http.status == 200) {
-					callback(JSON.parse(http.responseText))
-				} else {
-					parent.date.setDate(parent.date.getDate() - 1)
-					http.open("GET", `http://api.nbp.pl/api/exchangerates/tables/A/${TableNBP.format(parent.date)}/`);
-					http.send();
+					parent.mediator.notify("chart", JSON.parse(http.responseText))
 				}
 			}
 		}
 		
-		http.send();*/
+		http.send();
 	}
 	
 }
--- src/Mediator.js
+++ src/Mediator.js
@@ -10,11 +10,19 @@ class Mediator {
 		this.right = source.getElementById('dateRight')
 		this.right.onchange = function () {parent.notify("right", parent.right.value)}
 		this.chart = new Chart(parent);
-		this.c = source.getElementById('can').getContext('2d')
+		this.canvas = source.getElementById('can')
+		this.context = source.getElementById('can').getContext('2d')
+		this.select = source.getElementById('currencySelect')
+		this.select.onchange = function () {parent.notify('select', parent.select.value)}
+		this.code = parent.select.value
 	}
 	
 	notify(obj, message){
 		switch (obj){
+			case "select":
+				this.code = message
+				this.getChart()
+			break;
 			case "left":
 				this.start = message
 				this.getChart()
@@ -24,19 +32,33 @@ class Mediator {
 				this.getChart()
 			break;
 			case "chart":
-				
-				/*var height = 150
-				var max = 5
-				var column = 3.14 / max * height
-				c.fillRect(0, height - column, 30, column)*/
+				let width = this.canvas.width;
+				let height = this.canvas.height;
+				this.context.clearRect(0, 0, width, height);
+				let max = message.rates[0].mid
+				let min = message.rates[0].mid
+				for (let i = 1; i < message.rates.length; ++i){
+					if (message.rates[i].mid > max){
+						max = message.rates[i].mid
+					}
+					if (message.rates[i].mid < min){
+						min = message.rates[i].mid
+					}
+				}
+				let scale = min * 0.99
+				max -= scale 
+				let size = width / message.rates.length
+				for (let i = 0; i < message.rates.length; ++i){
+					let column = (message.rates[i].mid - scale) / max * height * 0.9
+					this.context.fillRect(i * size + (size * 0.1), height - column, size * 0.8, column)
+				}
 			break;
 		}
 	}
 	
 	getChart(){
-		if (this.start != undefined && this.end != undefined){
-			this.chart.get(this.start, this.end, "USD")
+		if (this.start != undefined && this.end != undefined && this.code != undefined){
+			this.chart.get(this.start, this.end, this.code)
 		}
 	}
-
 }
