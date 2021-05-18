3643fd5f53e7357bf628198aada80fd545df4c30
DawidMoza <dawidmoza@gmail.com>
2021-05-18 19:51:17 +0200

    charts and stuff

--- src/Mediator.js
+++ src/Mediator.js
@@ -2,8 +2,6 @@
 class Mediator {
 	
 	constructor (source){
-		this.start = undefined
-		this.end = undefined
 		this.left = source.getElementById('dateLeft')
 		let parent = this
 		this.left.onchange = function () {parent.notify("left", parent.left.value)}
@@ -15,6 +13,10 @@ class Mediator {
 		this.select = source.getElementById('currencySelect')
 		this.select.onchange = function () {parent.notify('select', parent.select.value)}
 		this.code = parent.select.value
+		this.span = source.getElementById('dateSpan')
+		this.start = this.left.value
+		this.end = this.right.value
+		this.getChart()
 	}
 	
 	notify(obj, message){
@@ -32,25 +34,30 @@ class Mediator {
 				this.getChart()
 			break;
 			case "chart":
-				let width = this.canvas.width;
-				let height = this.canvas.height;
-				this.context.clearRect(0, 0, width, height);
-				let max = message.rates[0].mid
-				let min = message.rates[0].mid
-				for (let i = 1; i < message.rates.length; ++i){
-					if (message.rates[i].mid > max){
-						max = message.rates[i].mid
+				if (message != false){
+					this.span.innerHTML = ""
+					let width = this.canvas.width;
+					let height = this.canvas.height;
+					this.context.clearRect(0, 0, width, height);
+					let max = message.rates[0].mid
+					let min = message.rates[0].mid
+					for (let i = 1; i < message.rates.length; ++i){
+						if (message.rates[i].mid > max){
+							max = message.rates[i].mid
+						}
+						if (message.rates[i].mid < min){
+							min = message.rates[i].mid
+						}
 					}
-					if (message.rates[i].mid < min){
-						min = message.rates[i].mid
+					let scale = min * 0.99
+					max -= scale 
+					let size = width / message.rates.length
+					for (let i = 0; i < message.rates.length; ++i){
+						let column = (message.rates[i].mid - scale) / max * height * 0.9
+						this.context.fillRect(i * size + (size * 0.1), height - column, size * 0.8, column)
 					}
-				}
-				let scale = min * 0.99
-				max -= scale 
-				let size = width / message.rates.length
-				for (let i = 0; i < message.rates.length; ++i){
-					let column = (message.rates[i].mid - scale) / max * height * 0.9
-					this.context.fillRect(i * size + (size * 0.1), height - column, size * 0.8, column)
+				} else {
+					this.span.innerHTML = "Brak danych dla wybranego zakresu, lub zakres przekracza 93 dni."
 				}
 			break;
 		}
@@ -62,3 +69,6 @@ class Mediator {
 		}
 	}
 }
+
+var module = module || {};
+module.exports = Mediator;
