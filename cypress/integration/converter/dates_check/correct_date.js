2a8fd94fd9fe84c9509faac426c81d79d97abf9b
wojtekweg <ojtek@outlook.com>
2021-06-11 11:09:09 +0200

    disabled waiting in tests

--- cypress/integration/converter/dates_check/correct_date.js
+++ cypress/integration/converter/dates_check/correct_date.js
@@ -15,24 +15,24 @@ describe("[IO-18] Exchange for correct date", function () {
     });
     })
 
-    it('converts 100 USD to PLN one week ago', function () {
-        select_date(7);
+    it('converts 100 USD to PLN one day ago', function () {
+        select_date(1);
 
-        cy.wait(1000)
+        //cy.wait(1000)
         test_converter('USD', 'PLN', 100, 4)
     });
 
     it('converts 100 EUR to PLN two and a half year ago', function () {
         select_date(180, 2);
 
-        cy.wait(1000)
+        //cy.wait(1000)
         test_converter('EUR', 'PLN', 100, 5)
     });
 
     it('converts 100 CHF to PLN ten years ago', function () {
         select_date(0, 10);
 
-        cy.wait(1000)
+        //cy.wait(1000)
         test_converter('CHF', 'PLN', 100, 5)
     });
 
